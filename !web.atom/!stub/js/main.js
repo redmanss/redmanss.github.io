ce51f296ec86b8581c7b6c8368528a44b4776363
redmanss <37474215+redmanss@users.noreply.github.com>
2018-05-23 15:42:31 +0300

    update

--- !web.atom/!stub/js/main.js
+++ !web.atom/!stub/js/main.js
@@ -5,3 +5,25 @@ $(window).on('load', function () {
     $spinner.fadeOut();
     $preloader.delay(1500).fadeOut('slow');
 });
+// VIDEO ------------------
+var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
+	
+	if (isIOS) {
+ 
+		var canvasVideo = new CanvasVideoPlayer({
+			videoSelector: '.video',
+			canvasSelector: '.canvas',
+			timelineSelector: false,
+			autoplay: true,
+			makeLoop: true,
+			pauseOnClick: false,
+			audio: false
+		});
+		
+	}else {
+		
+		// Use HTML5 video
+		document.querySelectorAll('.canvas')[0].style.display = 'none';
+		
+	}	   
+// END VIDEO ---------------

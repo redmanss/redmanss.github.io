3e97117787624c729740d866be88b51084e4499a
redmanss <37474215+redmanss@users.noreply.github.com>
2018-08-21 13:37:34 +0300

    up

--- !web.atom/!stub/js/main.js
+++ !web.atom/!stub/js/main.js
@@ -1,33 +1,5 @@
-$(function() {
-	"use strict";
-//STATS
-	var show = true;
-	var countbox = ".text-proposition";
-	$(window).on("scroll load resize", function(){
-		if(!show) {
-			return false; 						  // Отменяем показ анимации, если она уже была выполнена
-		}                  
-		var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
-		var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
-		var w_height = $(window).height();        // Высота окна браузера
-		var d_height = $(document).height();      // Высота всего документа
-		var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-		if(w_top + 0 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
-		$(".stats-number1").spincrement({
-			to: 367,
-			duration: 10000
-		});
-		$(".stats-number2").spincrement({
-		to: 25,
-		duration: 10000
-		});
-		$(".stats-number3").spincrement({
-		to: 7000,
-		duration: 10000
-		});
-			show = false;
-		}
-	});
+(function() {
+	
 // VIDEO
 var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
 	if (isIOS) {
@@ -234,18 +206,45 @@ else {
 	$('.close-menu').click(function(){
 		$('.drop-menu').toggle("slide", { direction: "right" },500);
 	});
-});
-// preloader
+//STATS
+	var show = true;
+	var countbox = ".text-proposition";
+	$(window).on("scroll load resize", function(){
+		if(!show) {
+			return false; 						  // Отменяем показ анимации, если она уже была выполнена
+		}                  
+		var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
+		var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
+		var w_height = $(window).height();        // Высота окна браузера
+		var d_height = $(document).height();      // Высота всего документа
+		var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+		if(w_top + 0 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+		$(".stats-number1").spincrement({
+			to: 367,
+			duration: 10000
+		});
+		$(".stats-number2").spincrement({
+		to: 25,
+		duration: 10000
+		});
+		$(".stats-number3").spincrement({
+		to: 7000,
+		duration: 10000
+		});
+			show = false;
+		}
+	});
+}());
+/*// preloader
 $(window).on('load', function () {
 	"use strict";
     var $preloader = $('#page-preloader'),
         $spinner   = $preloader.find('.spinner');
     $spinner.fadeOut();
     $preloader.delay(1000).fadeOut('slow');
-});
+});*/
 //Hide, click
 $(document).mouseup(function (e) {
-	"use strict";
     var container = $(".drop-language");
 	var language = $(".language-now, .arrow");
     if (container, language.has(e.target).length === 0)

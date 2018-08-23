0bcfa620e4e2aac09948f27795390d361b2d5f4e
redmanss <37474215+redmanss@users.noreply.github.com>
2018-08-23 17:46:56 +0300

    up

--- !web.atom/!stub/js/main.js
+++ !web.atom/!stub/js/main.js
@@ -235,32 +235,4 @@ $('.product-list').slick({
 	$('.close-menu').click(function(){
 		$('.drop-menu').toggle("slide", { direction: "right" },500);
 	});
-//STATS
-	var show = true;
-	var countbox = ".text-proposition";
-	$(window).on("scroll load resize", function(){
-		if(!show) {
-			return false; 						  // Отменяем показ анимации, если она уже была выполнена
-		}                  
-		var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
-		var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
-		var w_height = $(window).height();        // Высота окна браузера
-		var d_height = $(document).height();      // Высота всего документа
-		var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-		if(w_top + 0 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
-		$(".stats-number1").spincrement({
-			to: 367,
-			duration: 10000
-		});
-		$(".stats-number2").spincrement({
-		to: 25,
-		duration: 10000
-		});
-		$(".stats-number3").spincrement({
-		to: 7000,
-		duration: 10000
-		});
-			show = false;
-		}
-	});
 }());

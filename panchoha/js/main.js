568c5c64ed6f01b2b6339132aa4e36eb922342eb
redmanss <37474215+redmanss@users.noreply.github.com>
2019-06-07 17:37:33 +0300

    up

--- panchoha/js/main.js
+++ panchoha/js/main.js
@@ -105,22 +105,28 @@ $(function(){
 //hover header "desire"
     $('.block-header-desire').hover(function(){
         $('.header-desire').css({
-            background: 'url("img/icon/desire-hv.svg") no-repeat center'
+            backgroundPosition: '-26px -21px'
         });
     }, function(){
         $('.header-desire').css({
-            background: 'url("img/icon/desire.svg") no-repeat center'
+            backgroundPosition: '-58px 0'
         });
+    }).click(function () {
+        $('.desire-block').show().toggleClass('desire-hover');
+        $('.white-background').fadeIn(150);
     });
 //hover header "basket"
     $('.block-header-basket').hover(function(){
         $('.header-basket').css({
-            background: 'url("img/icon/basket-hv.svg") no-repeat center'
+            backgroundPosition: '-44px -21px'
         });
     }, function(){
         $('.header-basket').css({
-            background: 'url("img/icon/basket.svg") no-repeat center'
+            backgroundPosition: '-77px 0'
         });
+    }).click(function () {
+        $('.basket-block').show().toggleClass('basket-hover');
+        $('.white-background').fadeIn(150);
     });
 //hover header "login"
     $('.block-header-login').hover(function(){
@@ -141,9 +147,19 @@ $(function(){
         $('.login-block').show().toggleClass('login-hover');
         $('.white-background').fadeIn(150);
     });
-//close login
+//close pop
     $('.close-login').click(function(){
         $('.login-block').hide().toggleClass('login-hover');
+        $('.white-background').fadeOut(150);
+    });
+    //
+    $('.close-desire').click(function(){
+        $('.desire-block').hide().toggleClass('desire-hover');
+        $('.white-background').fadeOut(150);
+    });
+    //
+    $('.close-basket').click(function(){
+        $('.basket-block').hide().toggleClass('basket-hover');
         $('.white-background').fadeOut(150);
     });
 // search sub
@@ -159,5 +175,8 @@ $(function(){
             searchBlock.fadeOut(150);
         }
     });
-
+//remove-desire
+    $('.remove-desire').click(function () {
+        $(this).parents('.desire-list-product').remove();
+    });
 });

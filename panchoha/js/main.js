6096842770bf40acc3661a2498fbceeecadd6800
redmanss <37474215+redmanss@users.noreply.github.com>
2019-06-19 19:07:44 +0300

    up

--- panchoha/js/main.js
+++ panchoha/js/main.js
@@ -252,9 +252,19 @@ $(function(){
         $(".plus").show();
     });
     //
-// slider home
+// main slider home
     $(".slider").slick({
         prevArrow: ".arrow-back-bl",
         nextArrow: ".arrow-next-bl"
+    });
+// SliderPopular
+    $(".slider-popular").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots:true,
+        dotsClass: "dots-pop-slider",
+        prevArrow: ".pop-arrow-back-bl",
+        nextArrow: ".pop-arrow-next-bl"
     });
 });

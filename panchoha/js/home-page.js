318066293219c73446fe672f6c514cd9f2941605
redmanss <37474215+redmanss@users.noreply.github.com>
2019-07-08 08:58:08 +0300

    up

--- panchoha/js/home-page.js
+++ panchoha/js/home-page.js
@@ -12,6 +12,48 @@ $(function () {
         dots:true,
         dotsClass: "dots-pop-slider",
         prevArrow: ".pop-arrow-back-bl",
-        nextArrow: ".pop-arrow-next-bl"
+        nextArrow: ".pop-arrow-next-bl",
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+// NEW Block
+    $(".slider-product-block, .slider-sale-block").slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots:false,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
     });
 });

e04ebb78383b4cfc5801409439965dcc886679dc
redmanss <37474215+redmanss@users.noreply.github.com>
2019-09-12 00:49:23 +0300

    up

--- energywater/js/main.js
+++ energywater/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".header-block, .footer-menu").on("click","a", function (event) {
+    $(".header-block, .footer-menu, .mobile-menu").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
@@ -8,21 +8,21 @@ $(document).ready(function(){
 });
 //
 $(function () {
-    $('.works-slider-h').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        nextArrow: '.next-h',
-        prevArrow: '.prev-h',
-        infinite: false,
-    });
-    //
-    $('.works-slider-w').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        nextArrow: '.next-w',
-        prevArrow: '.prev-w',
-        infinite: false,
-    });
+    // $('.works-slider-h').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     nextArrow: '.next-h',
+    //     prevArrow: '.prev-h',
+    //     infinite: false,
+    // });
+    // //
+    // $('.works-slider-w').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     nextArrow: '.next-w',
+    //     prevArrow: '.prev-w',
+    //     infinite: false,
+    // });
     //
     $('.works-slider-s').slick({
         slidesToShow: 3,
@@ -30,14 +30,43 @@ $(function () {
         nextArrow: '.next-s',
         prevArrow: '.prev-s',
         infinite: false,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2
+                }
+        },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+
+        ]
     });
     //
-    $('.ts-slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        nextArrow: '.next-ts',
-        prevArrow: '.prev-ts',
-        infinite: false,
-        variableWidth: true,
+    // $('.ts-slider').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     nextArrow: '.next-ts',
+    //     prevArrow: '.prev-ts',
+    //     infinite: false,
+    //     variableWidth: true,
+    // });
+    //
+    $(".burger-menu").click(function () {
+        $(".mobile-menu").animate({
+            right: "0",
+        });
+        $(".background").fadeIn();
+    });
+    //
+    $(".close-menu").click(function () {
+        $(".mobile-menu").animate({
+            right: "-360px",
+        });
+        $(".background").fadeOut();
     });
 });

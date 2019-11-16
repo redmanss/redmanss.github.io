1fc4f8c8f3564f41022032fc6a2026ec4a0475d4
redmanss <37474215+redmanss@users.noreply.github.com>
2019-11-16 20:23:05 +0200

    up

---
+++ sowgrow/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.ph-slider-block').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: ".right-arrow",
+        prevArrow: ".left-arrow"
+    });
+});

3ef8995ba71c92db97c5234742f7668bd90250b5
redmanss <37474215+redmanss@users.noreply.github.com>
2019-11-16 23:54:28 +0200

    up

--- sowgrow/js/main.js
+++ sowgrow/js/main.js
@@ -1,3 +1,22 @@
+$(document).ready(function(){
+    $(".main-page-menu, .footer-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
+//
+$(document).on('click', '.order-button, .products-item-button', function () {
+    $('.consultation').fadeIn();
+    $(".background").fadeIn();
+});
+//
+$(document).on('click', '.close-pop-up', function() {
+    $(this).parent().fadeOut();
+    $(".background").fadeOut();
+});
+//
 $(function () {
     $('.ph-slider-block').slick({
         infinite: false,
@@ -5,5 +24,12 @@ $(function () {
         slidesToScroll: 1,
         nextArrow: ".right-arrow",
         prevArrow: ".left-arrow"
+    });
+    $('.products-item').hover(function () {
+        $(this).find(".img-product").hide();
+        $(this).find(".img-hover").show();
+    }, function () {
+        $(this).find(".img-product").show();
+        $(this).find(".img-hover").hide();
     });
 });

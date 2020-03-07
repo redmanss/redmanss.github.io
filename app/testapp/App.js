897a7a5da1453cc5a542dfa0f77f46bdba862e4a
redmanss <37474215+redmanss@users.noreply.github.com>
2020-03-07 09:09:35 +0200

    up

--- app/testapp/App.js
+++ app/testapp/App.js
@@ -1,13 +1,28 @@
+// In App.js in a new project
+
 import React from 'react'
-import {View, Text} from 'react-native'
-import {Navbar} from './src/Components/Navbar'
+import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-
-export default function App() {
+function HomeScreen() {
   return (
-    <View>
-      <Text>123</Text>
-      <Navbar />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
-  )
+  );
 }
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;

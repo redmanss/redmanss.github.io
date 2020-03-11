d9c65a5526154cf0b72624c863535c101aa2ff12
redmanss <37474215+redmanss@users.noreply.github.com>
2020-03-11 23:43:38 +0200

    up

--- app/projects/ptapp/src/screens/HomeScreen.js
+++ app/projects/ptapp/src/screens/HomeScreen.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 
-const fetchData = () => {
-    const response = fetch('', {
-        method: 'GET',
-        headers:{'Content-Type': 'application/json'}
-    })
-    const data = response.json()
-}
+// const fetchData = () => {
+//     const response = fetch('', {
+//         method: 'GET',
+//         headers:{'Content-Type': 'application/json'}
+//     })
+//     const data = response.json()
+// }
 
 export const HomeScreen = ({ navigation }) => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-             <Text>12</Text>
+        <View style={styles.navbar}>
+            <Text>Home Screen</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    navbar: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1
+    }
+})
+
+
---
+++ app/projects/ptapp/src/theme.js
@@ -0,0 +1,4 @@
+export const THEME = {
+    WHITE_COLOR: '#fff',
+    BG_COLOR: '#0071ba'
+}

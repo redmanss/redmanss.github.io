5f0797aa04ed079721125c9c598c8706105589d5
redmanss <37474215+redmanss@users.noreply.github.com>
2020-03-11 21:46:52 +0200

    up

--- app/projects/ptapp/src/screens/HomeScreen.js
+++ app/projects/ptapp/src/screens/HomeScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+const fetchData = () => {
+    const response = fetch('', {
+        method: 'GET',
+        headers:{'Content-Type': 'application/json'}
+    })
+    const data = response.json()
+}
+
 export const HomeScreen = ({ navigation }) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+             <Text>12</Text>
         </View>
     )
 }

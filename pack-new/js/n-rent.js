fd7cf3ba0cf3c7710075901b3272444a19f31a79
redmanss <37474215+redmanss@users.noreply.github.com>
2020-04-20 23:47:30 +0300

    up

---
+++ pack-new/js/n-rent.js
@@ -0,0 +1,10 @@
+//
+$(".chb-order").click(function() {
+    $(".n-consultation-block").show();
+    $("body").addClass("block-body");
+});
+//
+$(".close-ncb").click(function() {
+    $(".n-consultation-block").hide();
+    $("body").removeClass("block-body");
+});

dfad76cca75fe6fae8521ba571c8ece0b31dd687
redmanss <37474215+redmanss@users.noreply.github.com>
2020-04-21 15:28:25 +0300

    up

--- pack-new/js/n-rent.js
+++ pack-new/js/n-rent.js
@@ -8,3 +8,25 @@ $(".close-ncb").click(function() {
     $(".n-consultation-block").hide();
     $("body").removeClass("block-body");
 });
+//
+    $(window).scroll(function() {
+        let windowsHeight = $(window).height();
+        let top = $(document).scrollTop();
+        if (top > windowsHeight) 
+        {
+            $(".chb-header2").fadeIn();
+        }
+        else {
+            $(".chb-header2").fadeOut();
+        }
+    });
+//
+$(document).ready(function(){
+    $(".ch-menu a").on("click", function(e){
+        let anchor = $(this);
+        $('html, body').stop().animate({scrollTop: $(anchor.attr('href')).offset().top}, 777);
+        e.preventDefault();
+        return false;
+        });
+    });
+    

f96dd7ba03a54dbaec99d3a96f3a90d291ec082a
redmanss <37474215+redmanss@users.noreply.github.com>
2020-04-27 18:03:40 +0300

    up

--- pack-new/js/n-product-list.js
+++ pack-new/js/n-product-list.js
@@ -1,13 +1,5 @@
 $(function () {
 //
-    $(".name-filter").click(function () {
-        $(this).toggleClass("name-filter-a");
-    });
-//
-    $(".cbx-bg").click(function () {
-        $(this).next().toggleClass("name-filter-a");
-    });
-//
     $(".filter-title").click(function () {
         $(this).find(".plus").slideToggle(100);
         $(this).parent().find(".filter-box-list").slideToggle(200);

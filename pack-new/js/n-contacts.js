c3186ae6a4d402fa2d09a95d124eb60fec4d8995
redmanss <37474215+redmanss@users.noreply.github.com>
2020-06-02 17:36:53 +0300

    up

--- pack-new/js/n-contacts.js
+++ pack-new/js/n-contacts.js
@@ -1,11 +1,24 @@
-$(".cp-manager-tabs div").click(function () {
-    let indexdata = $(this).data("numbercontacts");
-    $(".cp-manager-tabs div").removeAttr("id");
-    $(this).attr("id", "active-cp-manager");
-    $(".cp-manager-item-block").css({
-        display: 'none'
+$(function(){
+    //
+    $(".cp-manager-tabs div").click(function () {
+        let indexdata = $(this).data("numbercontacts");
+        $(".cp-manager-tabs div").removeAttr("id");
+        $(this).attr("id", "active-cp-manager");
+        $(".cp-manager-item-block").css({
+            display: 'none'
+        });
+        $(".cp-manager-item-list .cp-manager-item-block:nth-child("+ indexdata +")").css({
+            display: 'flex'
+        });
     });
-    $(".cp-manager-item-list .cp-manager-item-block:nth-child("+ indexdata +")").css({
-        display: 'flex'
-    });
+    //
+    if ($(window).width() <=767) {
+        $(".cp-manager-tabs-block").click(function(){
+            $(this).find(".cp-manager-tabs").slideToggle();
+        });
+        $(".cp-manager-tabs div").click(function(){
+            let test = $(this).html();
+            $(".cp-manager-tabs-button").empty().html(test);
+        });
+    }
 });

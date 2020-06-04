23b5f7b63af08883b85365ddbf7d5c276a76f3a0
redmanss <37474215+redmanss@users.noreply.github.com>
2020-06-04 09:19:24 +0300

    up

---
+++ pack-new/js/options-fancybox.js
@@ -0,0 +1,22 @@
+$(function() {
+    // FANCYBOX OPTION
+$('[data-fancybox="gallery"]').fancybox({
+        thumbs : {
+            autoStart : false,
+        },
+        buttons: [
+            "zoom",
+            //"share",
+            //"slideShow",
+            //"fullScreen",
+            //"download",
+            "thumbs",
+            "close",
+        ],
+        backFocus: false,
+        autoFocus: false,
+        trapFocus: false,
+        loop: true,
+    });
+//
+});

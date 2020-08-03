48f4b8ec88c26ed1748c7b706e12a48ccaac4c76
redmanss <37474215+redmanss@users.noreply.github.com>
2020-08-03 23:40:43 +0300

    up

--- app/projects/ptapp/src/screens/HomeScreen.js
+++ app/projects/ptapp/src/screens/HomeScreen.js
@@ -6,38 +6,49 @@ export const HomeScreen = ({ navigation }) => {
 
 
     // const sendToBase = () => {
-    //      fetch('https://packtradeapp.firebaseio.com/base.json', {
+    //      fetch('https://packtradeapp.firebaseio.com/telehandlers.json', {
     //         method: 'POST',
     //         headers: {
     //             Accept: 'application/json',
     //             'Content-Type': 'application/json'
     //           },
     //         body: JSON.stringify({
-    //             firstParam: '1',
-    //             secondParam: '2'
+    //             title: 'MANITOU MT625 с.н.952121 2015  р.',
+    //             model: 'MT625',
+    //             invnumber: '2191',
+    //             enginehours: '3294.2',
+    //             engine: '55.4',
+    //             height: '1.9',
+    //             weight: '4500',
+    //             year: '2015'
     //         })
     //     })
     // }
 
+
+
+    // useEffect(() => {
+    //   fetch('https://packtradeapp.firebaseio.com/telehandlers.json')
+    //     .then((response) => response.json())
+    //     .then((json) => setData(json.telehandlers))
+    //     .catch((error) => console.error(error))
+    //     .finally(() => setLoading(false));
+    // }, []);
+
+
+
+
+    //const loadData = useCallback(async () => await giveOnBase(), [giveOnBase])
+
+    useEffect(() => {
+      giveOnBase()
+    }, [])
+
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
 
-    useEffect(() => {
-      fetch('https://reactnative.dev/movies.json')
-        .then((response) => response.json())
-        .then((json) => setData(json.movies))
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
-    }, []);
-
-    // const loadData = useCallback(async () => await giveOnBase(), [giveOnBase])
-
-    // useEffect(() => {
-    //   loadData()
-    // }, [])
-    
     const giveOnBase = async () => {
-        const response = await fetch('https://packtradeapp.firebaseio.com/base.json', {
+        const response = await fetch('https://packtradeapp.firebaseio.com/telehandlers.json', {
             method: 'GET',
             headers: {
                 Accept: 'application/json',
@@ -45,32 +56,30 @@ export const HomeScreen = ({ navigation }) => {
               },
         })
         const data = await response.json();
-        const parseData = Object.keys(data).map(key => ({ ...data[key], id:key}));
-        // console.log('firebase', parseData);
+        const arrayData = Object.keys(data).map(key => ({...data[key], id:key}));
+        //console.log(arrayData)
     }
+    
 
     return (
           <View style={{ flex: 1, padding: 24 }}>
                 {isLoading ? <ActivityIndicator/> : (
                   <FlatList
                     data={data}
-                    keyExtractor={({ id }, index) => id}
                     renderItem={({ item }) => (
-                      <Text>{item.title}, {item.releaseYear}</Text>
+                      <Text>{item.title}</Text>
                     )}
+                    keyExtractor={item => item.id}
                   />
                 )}
           </View>
 
 
         // <View style={styles.navbar}>
-        //     <Text>123</Text>
-        //     <TextInput>Ваш логин</TextInput>
-        //     <Button
-        //       onPress={giveOnBase}
-        //       title="Отправить"
-        //     />
-            
+        //     {/* <FlatList 
+        //       data={loadData}
+        //       renderItem={}
+        //     /> */}
         // </View>
         
     )

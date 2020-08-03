c8995a5354069ece03610249cd00743cbadf477c
redmanss <37474215+redmanss@users.noreply.github.com>
2020-08-03 18:42:01 +0300

    up

--- app/projects/ptapp/src/screens/HomeScreen.js
+++ app/projects/ptapp/src/screens/HomeScreen.js
@@ -1,64 +1,77 @@
 import React, { useEffect, useCallback, useState } from 'react'
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native'
+import { View, StyleSheet, Text, TextInput, Button, ActivityIndicator } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 
 export const HomeScreen = ({ navigation }) => {
 
 
-    const sendToBase = () => {
-         fetch('https://packtradeapp.firebaseio.com/base.json', {
-            method: 'POST',
+    // const sendToBase = () => {
+    //      fetch('https://packtradeapp.firebaseio.com/base.json', {
+    //         method: 'POST',
+    //         headers: {
+    //             Accept: 'application/json',
+    //             'Content-Type': 'application/json'
+    //           },
+    //         body: JSON.stringify({
+    //             firstParam: '1',
+    //             secondParam: '2'
+    //         })
+    //     })
+    // }
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      fetch('https://reactnative.dev/movies.json')
+        .then((response) => response.json())
+        .then((json) => setData(json.movies))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }, []);
+
+    // const loadData = useCallback(async () => await giveOnBase(), [giveOnBase])
+
+    // useEffect(() => {
+    //   loadData()
+    // }, [])
+    
+    const giveOnBase = async () => {
+        const response = await fetch('https://packtradeapp.firebaseio.com/base.json', {
+            method: 'GET',
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
               },
-            body: JSON.stringify({
-                firstParam: '1',
-                secondParam: '2'
-            })
         })
-    }
-    
-    const giveOnBase = async () => {
-        // const response = await fetch('https://packtradeapp.firebaseio.com/base.json', {
-        //     method: 'GET',
-        //     headers: {
-        //         Accept: 'application/json',
-        //         'Content-Type': 'application/json'
-        //       },
-        // })
-        // const data = await response.json();
-        // const parseData = Object.keys(data).map(key => ({ ...data[key], id:key}));
-        // //Sconsole.log('firebase', parseData);
-        // alert(parseData);
-
-
-        const getMoviesFromApiAsync = async () => {
-            try {
-              let response = await fetch(
-                'https://packtradeapp.firebaseio.com/base.json'
-              );
-              let json = await response.json();
-              return json.movies;
-            } catch (error) {
-              console.error(error);
-            }
-          };
-          
-
-
-        
+        const data = await response.json();
+        const parseData = Object.keys(data).map(key => ({ ...data[key], id:key}));
+        // console.log('firebase', parseData);
     }
 
     return (
-        <View style={styles.navbar}>
-            <Text>123</Text>
-            <TextInput>Ваш логин</TextInput>
-            <Button
-            onPress={giveOnBase}
-            title="Отправить"
-            />
+          <View style={{ flex: 1, padding: 24 }}>
+                {isLoading ? <ActivityIndicator/> : (
+                  <FlatList
+                    data={data}
+                    keyExtractor={({ id }, index) => id}
+                    renderItem={({ item }) => (
+                      <Text>{item.title}, {item.releaseYear}</Text>
+                    )}
+                  />
+                )}
+          </View>
+
+
+        // <View style={styles.navbar}>
+        //     <Text>123</Text>
+        //     <TextInput>Ваш логин</TextInput>
+        //     <Button
+        //       onPress={giveOnBase}
+        //       title="Отправить"
+        //     />
             
-        </View>
+        // </View>
         
     )
 }

0cc728abde27eded233b402ee7b955339c122a6e
redmanss <37474215+redmanss@users.noreply.github.com>
2020-09-29 18:55:19 +0300

    up

--- app/projects/ptapp/src/screens/DetailScreen.js
+++ app/projects/ptapp/src/screens/DetailScreen.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { View, Text } from "react-native"
+import React, { useState } from 'react'
+import { View, Text, Image, StyleSheet, FlatList, Modal, TouchableOpacity } from "react-native"
+import ImageViewer from 'react-native-image-zoom-viewer'
 
 export const DetailScreen = ({navigation, route}) => {
 
     const postArray = route.params.post
-
     const setTitle = () => navigation.setOptions({ title: postArray.inventory })
-
+    const [stateModal, setModal] = useState(false)
+   
     setTitle()
 
     return (
@@ -28,7 +29,46 @@ export const DetailScreen = ({navigation, route}) => {
             <Text>Шини: {postArray.tires}</Text>
             <Text>Ціна: {postArray.price}</Text>
             <Text>Примітки: {postArray.note}</Text>
+            <View style={styles.container}>
+                <FlatList
+                    data={postArray.imgblock}
+                    keyExtractor={ (item, index) => item + index}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity 
+                            onPress={()=> {setModal(true)}}
+                        >
+                            <Image
+                                style={styles.image}
+                                source={{ uri: item.url }}
+                            />
+                        </TouchableOpacity>
+                        
+                    )}
+                />
+            </View>
+            <Modal visible={stateModal} transparent={true} onRequestClose={() => {setModal(false)}}>
+                <ImageViewer 
+                    imageUrls={postArray.imgblock}
+                    enableSwipeDown={true}
+                    onCancel={() => {setModal(false)}}
+                />
+            </Modal>
         </View>
     )
-
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: '50%',
+        height: 100,
+    },
+    imageblock: {
+        width: '100%'
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+  });
+  
